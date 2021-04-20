704865a74257e46acee85d15477f82d0022c3fdd
Denis Abramov <DenAnger@outlook.com>
2021-04-20 16:41:28 +0700

    Initial commit

---
+++ Messenger/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Denis Abramov on 20.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Messenger/Supporting Files/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Messenger
+//
+//  Created by Denis Abramov on 20.04.2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+//    var window: UIWindow?
+//    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        return true
+//    }
+//
+//    // MARK: UISceneSession Lifecycle
+//
+//    @available(iOS 13.0, *)
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    @available(iOS 13.0, *)
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//    }
+}
