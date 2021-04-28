307bd3503f5d6d7b0395d585113d07751b86da9b
Denis Abramov <DenAnger@outlook.com>
2021-04-28 13:03:39 +0700

    StartViewController
    update pods

--- Messenger/Controllers/StartViewController.swift
+++ Messenger/Controllers/StartViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
+import Spring
 
 class StartViewController: UIViewController {
-
+    
+    @IBOutlet var signInButton: SpringButton! {
+        didSet {
+            signInButton.layer.cornerRadius = 10
+            
+            signInButton.animation = "Fade In"
+            signInButton.force = 2
+            signInButton.duration = 5
+            signInButton.delay = 0.3
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        signInButton.animate()
     }
 }
